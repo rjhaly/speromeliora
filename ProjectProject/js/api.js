d3ba1a6a3acb858eaa24e358cae628b9a8931ded
drphilllips <drphillips@wpi.edu>
2021-11-30 13:24:15 -0500

    sepppakkuuuiuuu

--- ProjectProject/js/api.js
+++ ProjectProject/js/api.js
@@ -2,8 +2,8 @@
 // be sure you change to accommodate your specific API Gateway entry point
 var base_url = "https://umrdxh6f76.execute-api.us-east-2.amazonaws.com/Alphiwalphi";
 
-var add_url    = base_url + "calculator";   // POST
-var list_url   = base_url + "projects";    // GET
-var create_url = base_url + "projects";    // POST
+var list_url   = base_url + "projectList";    // GET
+var create_url = base_url + "projectCreation";    // POST
+var get_url    = base_url + "project";
 
-var delete_url = base_url + "projects";    // POST with {name}   [challenge in getting DELETE to work]
+// var delete_url = base_url + "projects";    // POST with {name}   [challenge in getting DELETE to work]
