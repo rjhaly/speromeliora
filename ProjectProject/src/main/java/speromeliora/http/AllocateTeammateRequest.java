a0a4f5366c08654d6ed3ee425f697cd9ebe0687d
Reese <Reese@130.215.174.223>
2021-12-07 13:01:02 -0500

    allocate teammate?

---
+++ ProjectProject/src/main/java/speromeliora/http/AllocateTeammateRequest.java
@@ -0,0 +1,25 @@
+package speromeliora.http;
+
+public class AllocateTeammateRequest {
+	String arg1, arg2, arg3;
+	
+	public String getArg1() { return arg1; }
+	public String getArg2() { return arg2; }
+	public String getArg3() { return arg3; }
+	public void setArg1(String arg1) { this.arg1 = arg1; }
+	public void setArg2(String arg2) { this.arg2 = arg2; }
+	public void setArg3(String arg3) { this.arg3 = arg3; }
+
+	public String toString() {
+		return "Rename Task(" + arg1 + ")";
+	}
+	
+	public AllocateTeammateRequest (String arg1, String arg2, String arg3) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = arg3;
+	}
+	
+	public AllocateTeammateRequest() {
+	}
+}
